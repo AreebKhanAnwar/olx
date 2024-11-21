ee9e57cc1401d8cf7b842df210d84f46d32060ed
AreebKhanAnwar <areebkhanp1@gmail.com>
2024-11-22 00:05:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+var olx = [{ image: 'iphone.jpeg', tittle: "Iphone 16pro max", price: "Rs 7,00,000" },
+           {image: "Kawasaki.webp", tittle: "Kawasaki Z1000", price: "Rs 4,00,00,000"},
+           {image:"CG_125.webp", tittle:"Honda CG 125", price: "Rs 2,10,000"},
+           {image:"lenovo.jpg", tittle:"Lenovo X1 carbon i7 7th gen", price: "Rs 52,000"},
+           {image:"sorento.webp", tittle:"KIA Sorento 2.4 AWD 2021", price: "Rs 75,00,000"},
+           {image:"Mark_X.webp", tittle:"Toyota Mark X 300G Premium 2005", price: "Rs 60,00,000"},
+           {image:"pixel6a.webp", tittle:"Gooogle Pixel 6a", price: "Rs 86,499"},
+           {image:"dslr.webp", tittle:"Canon 60d With 75-300 Canon Lens", price: "Rs 50,000"},
+           {image:"ipad.webp", tittle:"Ipad Pro (M4)", price: "Rs 27,80,00"},
+        ];
+
+// CARD 1
+document.getElementById("iphone_price").innerHTML = olx[0].price;
+document.getElementById("iphone_name").innerHTML = olx[0].tittle;
+document.getElementById("iphone_img").src = olx[0].image;
+// CARD 2
+document.getElementById("kawasaki_price").innerHTML = olx[1].price;
+document.getElementById("kawasaki_name").innerHTML = olx[1].tittle;
+document.getElementById("kawaski_img").src = olx[1].image;
+// CARD 3
+document.getElementById("CG_price").innerHTML = olx[2].price;
+document.getElementById("CG_name").innerHTML = olx[2].tittle;
+document.getElementById("CG_img").src = olx[2].image;
+// CARD 4
+document.getElementById("lenovo_price").innerHTML = olx[3].price;
+document.getElementById("lenovo_name").innerHTML = olx[3].tittle;
+document.getElementById("lenovo_img").src = olx[3].image;
+// CARD 5
+document.getElementById("sorento_price").innerHTML = olx[4].price;
+document.getElementById("sorento_name").innerHTML = olx[4].tittle;
+document.getElementById("sorento_img").src = olx[4].image;
+// CARD 6
+document.getElementById("mark_price").innerHTML = olx[5].price;
+document.getElementById("mark_name").innerHTML = olx[5].tittle;
+document.getElementById("Mark_img").src = olx[5].image;
+// CARD 7
+document.getElementById("pixel_price").innerHTML = olx[6].price;
+document.getElementById("pixel_name").innerHTML = olx[6].tittle;
+document.getElementById("pixel_img").src = olx[6].image;
+// CARD 8
+document.getElementById("dslr_price").innerHTML = olx[7].price;
+document.getElementById("dslr_name").innerHTML = olx[7].tittle;
+document.getElementById("dslr_img").src = olx[7].image;
+// CARD 9
+document.getElementById("ipad_price").innerHTML = olx[8].price;
+document.getElementById("ipad_name").innerHTML = olx[8].tittle;
+document.getElementById("ipad_img").src = olx[8].image;
